de53596c914445003b1376e24f776d8cdecffe52
Melessa-mooh <melessa.iming@gmail.com>
2025-10-02 16:00:57 +0800

    chore: removed backend and frontend folders

--- backend/backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from app.views import hello_api
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/hello/', hello_api),
-]
