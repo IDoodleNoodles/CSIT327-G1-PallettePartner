f1e835260153631030fc35b06147d365ef71957e
LAB13_USER ON WS09 <L13X16W09@LabHCI.net>
2025-10-29 17:23:09 +0800

    bad

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
+python -m venv venv
+venv\Scripts\Activate.ps1
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py makemigrations
