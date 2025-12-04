b388f9ebeb6c42106e23277834d3764861f9eaf2
Melessa-mooh <melessa.iming@gmail.com>
2025-12-04 11:03:44 +0800

    Fix migration merge dependency; extract dashboard CSS/JS and reattach comment handlers

--- pallattepartner/pallate/migrations/0022_merge_20251204_0358.py
+++ pallattepartner/pallate/migrations/0022_merge_20251204_0358.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pallate', '0018_artwork_category'),
+        ('pallate', '0018_artwork_categories'),
         ('pallate', '0021_alter_artwork_categories'),
     ]
 
